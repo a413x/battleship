9b93b368b29d9f1efeb3642d6bb547a0d848092e
Alex <>
2020-09-07 14:39:33 +0300

    Cell component adjustment
    Changed parameter name and component styles

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 import '../styles/Cell.css'
 
-export const Cell = ({ isEmpty }) => {
+export const Cell = ({ isShip }) => {
   const [ cellClassName, setCellClassName ] = useState('cell')
   const [ cellContent, setCellContent ] = useState('')
 
   const onCellClick = (e) => {
     e.preventDefault()
     if(cellContent) return
-    const blinkType = isEmpty ? 'blink-blue' : 'blink-red'
+    const blinkType = isShip ? 'blink-red' : 'blink-blue'
     setCellClassName(cellClassName + ' ' + blinkType)
-    setCellContent('●')
+    setCellContent(isShip ? '╳' : '●')
   }
 
   return (
